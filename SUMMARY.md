ffbee217546305ff86db7867804061a9af250233
Phil Rosenfield <philrosenfield@gmail.com>
2017-07-29 09:50:08 -0400

    adding files and images

---
+++ SUMMARY.md
@@ -0,0 +1,11 @@
+# Summary
+
+* [Source Projections](sourceprojections.md)
+  * [Equirectangular Projection](sourceprojections.md#equirectangular-projection)
+  * [Mercator Projection](sourceprojections.md#mercator-projection)
+  * [Gnomonic Projection](sourceprojections.md#gnomonic-projection)
+* [TOAST Projection](toastprojection.md)
+  * [Approximating a Sphere](toastprojection.md#approximating-a-sphere)
+  * [TOAST Map of Earth](toastprojection.md#toast-map-of-earth)
+  * [Inside and Outside Projections](toastprojection.md#inside-and-outside-projections)
+  * [Image Pyramids](toastprojection.md#image-pyramids)
---
+++ images/images.sh
@@ -0,0 +1,15 @@
+wget wwtweb.blob.core.windows.net/docs/images/MapOfEarth.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast4.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast4Flat.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast16.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast16Flat.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast64.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast64Flat.jpg
+wget wwtweb.blob.core.windows.net/docs/images/ToastMapOfEarth.jpg
+wget wwtweb.blob.core.windows.net/docs/images/ToastLinks.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast4FlatInside.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast4FlatOutside.jpg
+wget wwtweb.blob.core.windows.net/docs/images/ToastPaths.jpg
+wget wwtweb.blob.core.windows.net/docs/images/toastedredplanet.jpg
+wget wwtweb.blob.core.windows.net/docs/images/Toast_Input.bmp
+wget wwtweb.blob.core.windows.net/docs/images/Toast_Output.bmp
